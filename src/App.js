8183210365a08636a63a23d6af11c01c6c837f12
shalini Kumarii <bittukumari@gmail.com>
2022-01-20 08:56:56 -0800

    Routing practice first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { useParams } from "react-router";
+
+const Home = (props) => {
+  return (
+    <h1 style={{ color: "red" }}>Welcome</h1>
+  );
+}
+
+const Hello = (props) => {
+  const { input } = useParams();
+
+  if (isNaN(input)) {
+      
+    
+  
+        return <p>The word is:{input}</p>
+    }
+
+  else {
+    return <p>The Number is:{input}</p>
+  }
+}
+
+  const Hello1= (props) => {
+    const { input,color1,color2 } = useParams();
+
+     return (<p style={{
+              color:`${color1}` ,
+              backgroundColor:`${color2}`,
+            }}>The Word is :{input}</p>);
+
+  }
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    
+    <Switch>
+      <Route exact path="/home">
+        <Home />
+      </Route>
+      <Route exact path="/:input">
+      <Hello />
+      </Route>
+      <Route  exact path="/:input/:color1/:color2">
+      <Hello1 />
+      </Route>
+    </Switch>
+    </BrowserRouter>
+ 
     </div>
   );
 }
 
 export default App;
+
